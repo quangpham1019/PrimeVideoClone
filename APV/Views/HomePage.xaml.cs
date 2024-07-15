140efba6e30442f7d2fa4905fbb1546d840ba20a
Quang Pham_ MSI <quangpham1019@gmail.com>
2024-07-15 15:30:39 -0700

    Add floating capability and placeholder spacing for the 2 top menus in HomePage.

--- APV/Views/HomePage.xaml.cs
+++ APV/Views/HomePage.xaml.cs
@@ -1,6 +1,7 @@
 using APV.CoreBusiness;
 using APV.ViewModels;
 using Microsoft.Maui.Animations;
+using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
 
 namespace APV.Views;
 
@@ -17,10 +18,11 @@ public partial class HomePage : ContentPage
 
     private async void ScrollView_Scrolled(object sender, ScrolledEventArgs e)
     {
+
         double curScrollY = e.ScrollY;
         bool menuIsVisible = label1.IsVisible;
 
-        if (Math.Abs(curScrollY - PrevScrollY) > 30)
+        if (Math.Abs(curScrollY - PrevScrollY) > 20)
         {
             // if scrolling down, curScrollY > prevScrollY && menu is visible
                 // hide menu, menu visible = false
@@ -29,26 +31,15 @@ public partial class HomePage : ContentPage
             if (curScrollY > PrevScrollY && menuIsVisible == true)
             {
                 label1.IsVisible = false;
-            } else if (curScrollY < PrevScrollY && menuIsVisible == false)
+                label2.IsVisible = false;
+            }
+            else if (curScrollY < PrevScrollY && menuIsVisible == false)
             {
                 label1.IsVisible = true;
+                label2.IsVisible = true;
             }
         }
 
-        await Console.Out.WriteLineAsync($"PrevScrollY: {PrevScrollY}");
         PrevScrollY = e.ScrollY;
-        await Console.Out.WriteLineAsync($"current event ScrollY: {e.ScrollY}");
-        await Console.Out.WriteLineAsync("---------");
-
-
-
-        //await label1.FadeTo(0, 1000, Easing.BounceOut);
-        //if (label1.IsVisible)
-        //{
-        //    label1.IsVisible = false;
-        //} else
-        //{
-        //    label1.IsVisible = true;
-        //}
     }
 }
