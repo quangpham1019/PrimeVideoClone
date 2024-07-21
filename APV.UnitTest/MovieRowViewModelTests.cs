356aaefa626b65dad8e8d36881553b47cf6e672e
Quang Pham_ MSI <quangpham1019@gmail.com>
2024-07-20 21:38:24 -0700

    Update UI for HomePage.

---
+++ APV.UnitTest/MovieRowViewModelTests.cs
@@ -0,0 +1,27 @@
+﻿using APV.ViewModels;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APV.UnitTest
+{
+    public class MovieRowViewModelTests
+    {
+        [Fact]
+        public void MovieRowViewModelParameterlessConstructorTest()
+        {
+            // Arrange
+            MovieRowViewModel viewModel = new MovieRowViewModel();
+
+            // Act
+
+            // Assert
+            Assert.NotNull(viewModel);
+            Assert.Null(viewModel.MovieList);
+            Assert.Null(viewModel.MovieRowHeading);
+        }
+    }
+}
