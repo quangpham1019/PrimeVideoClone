3538a2842d0251d6794cfe4f8956e001d4aea179
Quang Pham_ MSI <quangpham1019@gmail.com>
2024-08-21 18:55:16 -0700

    Refactor HomePageTest to use AutomationId as locator. Add test for menu interaction on swipe up, swipe down, swipe left on a movie row.

--- AppiumIntegrationTest/HomePageTest.cs
+++ AppiumIntegrationTest/HomePageTest.cs
@@ -1,4 +1,5 @@
 ﻿using AppiumIntegrationTest;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Interactions;
 using Assert = NUnit.Framework.Assert;
@@ -8,98 +9,140 @@ namespace APV.Test
     [TestFixture("14.0", "emulator-5556", 4724)]
     public class HomePageTest : AndroidBaseTest
     {
+        private readonly string scrollViewClassName = "android.widget.ScrollView";
 
         private readonly string carouselViewId = "com.companyname.apv:id/carouselView";
         private readonly string carouselViewIndicatorId = "com.companyname.apv:id/carouselViewIndicator";
+
         private readonly string topMenuShadowId = "com.companyname.apv:id/topMenuShadow";
         private readonly string topMenuId = "com.companyname.apv:id/topMenu";
+
         private readonly string movieRowCollectionViewId = "com.companyname.apv:id/movieRowCollectionView";
         private readonly string movieRowCollectionView_movieRowClass = "androidx.recyclerview.widget.RecyclerView";
+        private readonly string movieRowCollectionView_movieRow_posterClass = "android.widget.ImageView";
+
         private readonly string homePageNavigationBarId = "com.companyname.apv:id/homePageNavigationBar";
         private readonly string homePageItemFilterId = "com.companyname.apv:id/homePageItemFilter";
         //private readonly string Id = "";
+
+        private Interaction pressDown, releasePress, movePointerToScreenCenter;
 
         public HomePageTest(string platformVersion, string avdUdid, int port) : base(platformVersion, avdUdid, port) { }
 
         [Test]
         [Order(0)]
-        public void SwipeDownOnScreen()
+        public void SetUpVar()
         {
-            var carouselView = _driver.FindElement(MobileBy.Id(carouselViewId));
-            var carouselViewIndicator = _driver.FindElement(MobileBy.Id(carouselViewIndicatorId));
-            var topMenuShadow = _driver.FindElement(MobileBy.Id(topMenuShadowId));
-            var topMenu = _driver.FindElement(MobileBy.Id(topMenuId));
-            var movieRowCollectionView = _driver.FindElement(MobileBy.Id(movieRowCollectionViewId));
-            var movieRowCollectionView_movieRowList = movieRowCollectionView.FindElements(MobileBy.Id(movieRowCollectionView_movieRowClass));
-            var homePageNavigationBarList = _driver.FindElements(MobileBy.Id(homePageNavigationBarId));
-            var homePageItemFilterList = _driver.FindElements(MobileBy.Id(homePageItemFilterId));
+            //var carouselView = _driver.FindElement(MobileBy.Id(carouselViewId));
+            //var carouselViewIndicator = _driver.FindElement(MobileBy.Id(carouselViewIndicatorId));
 
+            //var topMenuShadow = _driver.FindElement(MobileBy.Id(topMenuShadowId));
+            //var topMenu = _driver.FindElement(MobileBy.Id(topMenuId));
 
+            //var movieRowCollectionView = _driver.FindElement(MobileBy.Id(movieRowCollectionViewId));
+            //var movieRowCollectionView_movieRowList = movieRowCollectionView.FindElements(MobileBy.Id(movieRowCollectionView_movieRowClass));
+
+            //var homePageNavigationBarList = _driver.FindElements(MobileBy.Id(homePageNavigationBarId));
+            //var homePageItemFilterList = _driver.FindElements(MobileBy.Id(homePageItemFilterId));
+
+            movePointerToScreenCenter = pointerInputDevice.CreatePointerMove(CoordinateOrigin.Viewport, windowWidth/2, windowHeight/2, TimeSpan.FromSeconds(1));
+            pressDown = pointerInputDevice.CreatePointerDown(pointerButton);
+            releasePress = pointerInputDevice.CreatePointerUp(pointerButton);
         }
 
         [Test]
         [Order(1)]
-        public void TopMenuDisappearOnScrollUp()
+        public void TopMenuDisappearOnQuickSwipeUp()
         {
+            // move pointer to center of screen, press, move up about 20px/s, release
+            PerformScroll(0, -windowHeight/2, 0.5);
 
+            // Assert that the topMenu no longer contains navigationBar and itemFilterList
+            var topMenu = _driver.FindElement(MobileBy.Id(topMenuId));
+            bool homePageItemFilterIsVisible = topMenu.FindElements(MobileBy.Id(homePageItemFilterId)).Count != 0;
+            bool homePageNavigationBarIsVisible = topMenu.FindElements(MobileBy.Id(homePageNavigationBarId)).Count != 0;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(homePageNavigationBarIsVisible, Is.False);
+                Assert.That(homePageItemFilterIsVisible, Is.False);
+            });
         }
 
         [Test]
         [Order(2)]
-        public void TopMenuReappearOnScrollDown()
+        public void TopMenuReappearOnQuickSwipeDown()
         {
+            // move pointer to center of screen, press, move down about 20px/s, release
+            PerformScroll(0, windowHeight/2, 0.5);
 
+            // Assert that the topMenu now contains navigationBar and itemFilterList
+            var topMenu = _driver.FindElement(MobileBy.Id(topMenuId));
+            bool homePageItemFilterIsVisible = topMenu.FindElements(MobileBy.Id(homePageItemFilterId)).Count != 0;
+            bool homePageNavigationBarIsVisible = topMenu.FindElements(MobileBy.Id(homePageNavigationBarId)).Count != 0;
+
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(homePageNavigationBarIsVisible, Is.True);
+                Assert.That(homePageItemFilterIsVisible, Is.True);
+            });
         }
 
 
 
         [Test]
-        [Order(4)]
+        [Order(3)]
         public void SwipeOnLastMovieRowInView()
         {
-            // TODO: swiping action need adjustment
-            string comedy_deadpoolWolverine_poster_XPath = "//android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.widget.ImageView";
-            string comedy_theGarfieldMovie_poster_XPath = "//android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup/android.widget.ImageView";
+            // find the movieRowCollectionView
+            // find the last recyclerView of movieRowCollectionView, record the elementId of the last element
+            // swipe left on the last recylerView
+            // Assert that the current last element of the last recyclerView has a different elementId
 
-            var calibrateCoordinate = pointerInputDevice.CreatePointerMove(CoordinateOrigin.Viewport, windowWidth / 2, windowHeight / 2, TimeSpan.Zero);
-            var pressDown = pointerInputDevice.CreatePointerDown(pointerButton);
-            var releasePress = pointerInputDevice.CreatePointerUp(pointerButton);
+            PerformScroll(0, -windowHeight / 2, 0.5);
 
-            var swipeVertical = pointerInputDevice.CreatePointerMove(CoordinateOrigin.Pointer, 0, -windowHeight, TimeSpan.FromSeconds(2));
-            ActionBuilder swipeDownToComedyGenre = new ActionBuilder().AddActions(
-                calibrateCoordinate,
+            var movieRowCollectionView = _driver.FindElement(MobileBy.Id(movieRowCollectionViewId));
+            var movieRowCollectionView_movieRowList = movieRowCollectionView.FindElements(MobileBy.ClassName(movieRowCollectionView_movieRowClass));
+
+            var lastPoster = movieRowCollectionView_movieRowList[^1]
+                .FindElements(MobileBy.ClassName(movieRowCollectionView_movieRow_posterClass))
+                .Last();
+            var lastPosterId = lastPoster.Id;
+
+            var mainAction = pointerInputDevice.CreatePointerMove(lastPoster, -windowWidth * 4 / 3, 0, TimeSpan.FromSeconds(2));
+            ActionBuilder swipeLeftOnMovieRow = new ActionBuilder().AddActions(
+                pointerInputDevice.CreatePointerMove(lastPoster, 0, 0, TimeSpan.FromSeconds(2)),
                 pressDown,
-                swipeVertical,
+                mainAction,
                 releasePress);
-            _driver.PerformActions(swipeDownToComedyGenre.ToActionSequenceList());
+            _driver.PerformActions(swipeLeftOnMovieRow.ToActionSequenceList());
 
-            var deadpoolWolverinePoster = _driver.FindElement(MobileBy.XPath(comedy_deadpoolWolverine_poster_XPath));
+            movieRowCollectionView_movieRowList = movieRowCollectionView.FindElements(MobileBy.ClassName(movieRowCollectionView_movieRowClass));
+            var lastPosterAfterSwipeId = movieRowCollectionView_movieRowList[^1]
+                .FindElements(MobileBy.ClassName(movieRowCollectionView_movieRow_posterClass))
+                .Last()
+                .Id;
 
-            var swipeLeftOnDeadpoolWolverinePoster = pointerInputDevice.CreatePointerMove(deadpoolWolverinePoster, -windowWidth*4/3, 0, TimeSpan.FromSeconds(2));
-
-            //var moveFingerToDeadpoolWolverinePoster = new Actions(_driver).MoveToElement(deadpoolWolverinePoster);
-            ActionBuilder swipeLeftOnComedyGenre = new ActionBuilder().AddActions(
-                pointerInputDevice.CreatePointerMove(deadpoolWolverinePoster, 0, 0, TimeSpan.FromSeconds(2)),
-                pressDown,
-                swipeLeftOnDeadpoolWolverinePoster,
-                releasePress);
-            _driver.PerformActions(swipeLeftOnComedyGenre.ToActionSequenceList());
-
-            var theGarfieldMoviePoster = _driver.FindElement(MobileBy.XPath(comedy_theGarfieldMovie_poster_XPath));
-            Assert.That(theGarfieldMoviePoster.Displayed, Is.True);
-
-            theGarfieldMoviePoster.Click();
-
-            var backgroundImage = _driver.FindElement(MobileBy.XPath("//android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.widget.ImageView"));
-
-            Assert.That(backgroundImage.Displayed);
+            Assert.That(lastPosterId, Is.Not.EqualTo(lastPosterAfterSwipeId));
         }
 
-        [Test]
-        [Order(5)]
-        public void Login()
-        {
+        //[Test]
+        //[Order(5)]
+        //public void Login()
+        //{
 
+        //}
+
+        void PerformScroll(int distanceX, int distanceY, double seconds)
+        {
+            var mainAction = pointerInputDevice.CreatePointerMove(CoordinateOrigin.Pointer, distanceX, distanceY, TimeSpan.FromSeconds(seconds));
+            ActionBuilder quickSwipeUp = new ActionBuilder().AddActions(
+                movePointerToScreenCenter,
+                pressDown,
+                mainAction,
+                releasePress);
+            _driver.PerformActions(quickSwipeUp.ToActionSequenceList());
         }
     }
 }
